6780a210ce100c2b4514ca802feaf8e3a439e035
Till Kuhn <kuhnibaert@gmail.com>
2015-06-15 18:12:27 +0200

    Add sample test to create resources for I18n

--- src/test/java/com/kuhniverse/it/CaptchaIntegrationTest.java
+++ src/test/java/com/kuhniverse/it/CaptchaIntegrationTest.java
@@ -1,24 +1,16 @@
 package com.kuhniverse.it;
 
-import com.kuhniverse.Application;
 import com.kuhniverse.domain.CaptchaFrontEndData;
 import com.kuhniverse.web.CaptchaController;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.inject.Inject;
 
 /**
  * Created by tillkuhn on 27.05.2015.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
-public class CaptchaIntegrationTest {
+public class CaptchaIntegrationTest extends BaseIntegrationTest{
 
     @Inject
     private CaptchaController controller;
